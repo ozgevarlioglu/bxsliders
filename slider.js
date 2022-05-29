e3a3920addd482da72d753cf281beee3e0cc07f8
ozgevarlioglu <83310285+ozgevarlioglu@users.noreply.github.com>
2022-05-29 23:24:08 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  shrinkItems: true,  
+			  randomStart: true,  
+			  pause: 3000, 
+			  captions:true,
+
+			});
+});
+
